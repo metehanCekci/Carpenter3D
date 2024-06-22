e0126b3a5828eb90bc817a1434555ee98a10eef4
integerfan66 <kaan27043@gmail.com>
2024-06-22 23:13:12 +0300

    kaymanın yavaş eğilmesi düzeltildi

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,24 +1,23 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 using System.Collections;
-using TMPro;
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float movementSpeed = 10f;
-    [SerializeField] float crouchSpeed = 5f;  // E�ilme s�ras�nda hareket h�z�
-    [SerializeField] float slideSpeed = 15f;  // Kayma s�ras�nda hareket h�z�
+    [SerializeField] float crouchSpeed = 5f;  
+    [SerializeField] float slideSpeed = 15f;  
     [SerializeField] float jumpForce = 10f;
     [SerializeField] float slamForce = 30f;
-    [SerializeField] float dashForce = 20f;   // At�lma s�ras�nda uygulanan kuvvet
-    [SerializeField] float dashCooldown = 1f; // At�lma i�in bekleme s�resi
-    [SerializeField]  float gravity = -9.81f;
+    [SerializeField] float dashForce = 20f;  
+    [SerializeField] float dashCooldown = 1f; 
+    [SerializeField] float gravity = -9.81f;
     [SerializeField] float crouchHeight = 0.7f;
-    [SerializeField] float dashCrouchHeight = 1.2f; // Dash sırasında crouch yüksekliği
+    [SerializeField] float dashCrouchHeight = 1.2f; 
     [SerializeField] float standingHeight = 2f;
     [SerializeField] private float slopeForceRayLength = 1.5f;
     [SerializeField] private float slopeDrag = 5f;
-    [SerializeField] private float fastFallMultiplier = 10f; // Havada h�zl� ini� i�in ek kuvvet �arpan�
+    [SerializeField] private float fastFallMultiplier = 10f; 
 
     [SerializeField] private float duration;
     [SerializeField] private float magnitude;
@@ -36,13 +35,13 @@ public class PlayerMovement : MonoBehaviour
     private bool isCrouching;
     private bool isSliding;
     [HideInInspector] public bool isDashing;
-    [HideInInspector] public bool canDash = true;  // At�lma eylemini yapabilme durumu
+    [HideInInspector] public bool canDash = true; 
     [HideInInspector] public bool isSlamming = false;
     private bool isWalking = false;
-    public bool hasAirDashed = false;  // Yeni değişken
+    public bool hasAirDashed = false;  
     private CapsuleCollider capsuleCollider;
     private Camera mainCamera;
-    private CameraShake cameraShake; // Reference to the CameraShake script
+    private CameraShake cameraShake; 
 
     public float attackDistance = 3f;
     public float attackDelay = 0.4f;
@@ -57,7 +56,7 @@ public class PlayerMovement : MonoBehaviour
     {
         inputActions = new PlayerInputActions();
         mainCamera = Camera.main;
-        cameraShake = mainCamera.GetComponent<CameraShake>(); // Initialize the camera shake reference
+        cameraShake = mainCamera.GetComponent<CameraShake>(); 
     }
 
     void OnEnable()
@@ -87,8 +86,8 @@ public class PlayerMovement : MonoBehaviour
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
-        rigidBody.freezeRotation = true;  // Rotation'u dondurarak stabiliteyi art�r
-        rigidBody.useGravity = true; // Yer�ekimini kullan
+        rigidBody.freezeRotation = true; 
+        rigidBody.useGravity = true; 
         capsuleCollider = GetComponent<CapsuleCollider>();
     }
 
@@ -99,87 +98,71 @@ public class PlayerMovement : MonoBehaviour
             Move();
             ApplyGravity();
         }
-
-
     }
 
     void Move()
     {
-        if (isDashing) return;  // Stop moving if dashing
-
-
+        if (isDashing) return; 
 
         float speed = isSliding ? slideSpeed : (isCrouching ? crouchSpeed : movementSpeed);
-        Vector3 move;
-
-        move = transform.right * moveInput.x + transform.forward * moveInput.y;
+        Vector3 move = transform.right * moveInput.x + transform.forward * moveInput.y;
 
         if (OnSlope())
         {
             move = Vector3.ProjectOnPlane(move, GetSlopeNormal());
             if (moveInput == Vector2.zero)
             {
-                rigidBody.drag = slopeDrag; // Increase drag when not moving
+                rigidBody.drag = slopeDrag; 
             }
             else
             {
-                rigidBody.drag = 0f; // Reset drag when moving
+                rigidBody.drag = 0f; 
             }
         }
-        else
+
+        if (isSliding)
         {
-            rigidBody.drag = 0f; // Reset drag on flat ground
+            targetVelocity = new Vector3(slideDirection.x * speed, rigidBody.velocity.y, slideDirection.z * speed);
+            rigidBody.velocity = Vector3.ClampMagnitude(targetVelocity, speed); 
         }
-        if (!isSliding)
+        else
         {
             targetVelocity = new Vector3(move.x * speed, rigidBody.velocity.y, move.z * speed);
-
-            //Sınırlamayı (clamp) kaldırarak daha hızlı düşüş sağlanıyor
-            rigidBody.velocity = targetVelocity; 
-            
-            //rigidBody.velocity = Vector3.ClampMagnitude(targetVelocity, speed);  Cap the speed to prevent excessive acceleration
-        }
-        else
-        {
-            rigidBody.velocity = Vector3.ClampMagnitude(slideDirection, speed); // Cap the speed to prevent excessive acceleration
+            rigidBody.velocity = targetVelocity;
         }
     }
 
     void ApplyGravity()
     {
-            rigidBody.AddForce(new Vector3(0, gravity, 0), ForceMode.Acceleration);
-        
+        rigidBody.AddForce(new Vector3(0, gravity, 0), ForceMode.Acceleration);
     }
 
     void OnMove(InputAction.CallbackContext context)
     {
         moveInput = context.ReadValue<Vector2>();
 
-
-
-            if (context.performed)
-            {
-                isWalking = true;
-                if(isGrounded)
+        if (context.performed)
+        {
+            isWalking = true;
+            if (isGrounded)
                 transform.GetChild(0).transform.GetChild(0).gameObject.GetComponent<Animator>().SetBool("isWalking", true);
-                else transform.GetChild(0).transform.GetChild(0).gameObject.GetComponent<Animator>().SetBool("isWalking", false);
-            }
-            else if(context.canceled)
-            {
+            else
+                transform.GetChild(0).transform.GetChild(0).gameObject.GetComponent<Animator>().SetBool("isWalking", false);
+        }
+        else if (context.canceled)
+        {
             transform.GetChild(0).transform.GetChild(0).gameObject.GetComponent<Animator>().SetBool("isWalking", false);
             isWalking = false;
-            }
-            else if (isGrounded)
-            {
-                transform.GetChild(0).transform.GetChild(0).gameObject.GetComponent<Animator>().SetBool("isWalking", false);
-                isWalking = false;
-            }
-            else
-            {
-                isWalking = false;
-            }
-
-
+        }
+        else if (isGrounded)
+        {
+            transform.GetChild(0).transform.GetChild(0).gameObject.GetComponent<Animator>().SetBool("isWalking", false);
+            isWalking = false;
+        }
+        else
+        {
+            isWalking = false;
+        }
     }
 
     void OnJump(InputAction.CallbackContext context)
@@ -190,16 +173,16 @@ public class PlayerMovement : MonoBehaviour
             {
                 transform.GetChild(0).transform.GetChild(0).gameObject.GetComponent<Animator>().SetBool("isWalking", false);
                 SfxScript.Instance.playJump();
-                rigidBody.velocity = new Vector3(rigidBody.velocity.x, 0, rigidBody.velocity.z); // Y ekseni hızını sıfırla
+                rigidBody.velocity = new Vector3(rigidBody.velocity.x, 0, rigidBody.velocity.z); 
                 rigidBody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
                 isJumping = true;
             }
-            else if (ReadJson.Instance.config.hasDoubleJump) // buraya
+            else if (ReadJson.Instance.config.hasDoubleJump)
             {
                 if (canDJump)
                 {
                     SfxScript.Instance.playJump();
-                    rigidBody.velocity = new Vector3(rigidBody.velocity.x, 0, rigidBody.velocity.z); // Y ekseni hızını sıfırla
+                    rigidBody.velocity = new Vector3(rigidBody.velocity.x, 0, rigidBody.velocity.z); 
                     rigidBody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
                     isJumping = true;
                     canDJump = false;
@@ -210,7 +193,6 @@ public class PlayerMovement : MonoBehaviour
 
     public void SlamImpact()
     {
-        // Implement effects here, like camera shake, damaging enemies, etc.
         SfxScript.Instance.playSlam();
         isSlamming = false;
         Debug.Log("Slam Impact!");
@@ -223,13 +205,11 @@ public class PlayerMovement : MonoBehaviour
             if (isGrounded && moveInput != Vector2.zero)
             {
                 isSliding = true;
-                isCrouching = true;
-                slideDirection = (transform.right * moveInput.x + transform.forward * moveInput.y).normalized * slideSpeed;
+                slideDirection = (transform.right * moveInput.x + transform.forward * moveInput.y).normalized ; // Use the player's forward direction
                 capsuleCollider.height = crouchHeight;
             }
             else if (!isGrounded)
             {
-                // Perform fast fall in the air
                 isSlamming = true;
                 rigidBody.AddForce(Vector3.down * fastFallMultiplier * slamForce, ForceMode.Impulse);
                 Debug.Log(rigidBody.velocity.y);
@@ -254,7 +234,7 @@ public class PlayerMovement : MonoBehaviour
         if (context.performed && canDash && !isDashing && (isGrounded || !hasAirDashed))
         {
             StartCoroutine(Dash());
-            if (!isGrounded) hasAirDashed = true;  // Havada dash yapıldığında işaretle
+            if (!isGrounded) hasAirDashed = true; 
         }
     }
 
@@ -309,9 +289,8 @@ public class PlayerMovement : MonoBehaviour
         if (hit.collider.gameObject.CompareTag("Hitable"))
         {
             hit.transform.gameObject.GetComponent<EnemyHealthScript>().takeDamage(attackDamage);
-            SfxScript.Instance.gameObject.GetComponent<AudioSource>().pitch = Random.Range(0.9f,1.1f);
+            SfxScript.Instance.gameObject.GetComponent<AudioSource>().pitch = Random.Range(0.9f, 1.1f);
             SfxScript.Instance.playHit();
-            //Time.timeScale = 0.05f;
             this.gameObject.transform.GetChild(0).transform.GetChild(0).gameObject.GetComponent<Animator>().speed = 0.01f;
             GameObject clone = Instantiate(bloodEffectPrefab);
             clone.SetActive(true);
@@ -323,7 +302,6 @@ public class PlayerMovement : MonoBehaviour
 
     private void recoverTime()
     {
-        //Time.timeScale = 1;
         this.gameObject.transform.GetChild(0).transform.GetChild(0).gameObject.GetComponent<Animator>().speed = 1;
         SfxScript.Instance.gameObject.GetComponent<AudioSource>().pitch = 1;
     }
@@ -331,35 +309,31 @@ public class PlayerMovement : MonoBehaviour
     private IEnumerator Dash()
     {
         isDashing = true;
-        canDash = false; // Atılma yapılamaz hale getir
+        canDash = false;
 
         SfxScript.Instance.playDash();
         Vector3 dashDirection = transform.right * moveInput.x + transform.forward * moveInput.y;
         if (dashDirection == Vector3.zero)
         {
-            dashDirection = transform.forward; // Default direction if no input
+            dashDirection = transform.forward; 
         }
 
-        // Change collider height for crouch effect
         capsuleCollider.height = dashCrouchHeight;
 
-        // Disable gravity for the duration of the dash
         rigidBody.useGravity = false;
-        rigidBody.velocity = Vector3.zero; // Reset velocity to ensure clean dash
+        rigidBody.velocity = Vector3.zero; 
         rigidBody.AddForce(dashDirection.normalized * dashForce, ForceMode.VelocityChange);
 
-        yield return new WaitForSeconds(0.1f);  // Atılma süresi
+        yield return new WaitForSeconds(0.1f); 
 
-        // Reset collider height after dash
         capsuleCollider.height = standingHeight;
 
-        // Enable gravity again
         rigidBody.useGravity = true;
 
         isDashing = false;
 
-        yield return new WaitForSeconds(dashCooldown);  // Atılma için bekleme süresi
-        canDash = true; // Atılma tekrar yapılabilir hale gelir
+        yield return new WaitForSeconds(dashCooldown); 
+        canDash = true; 
     }
 
     private bool OnSlope()
@@ -387,10 +361,6 @@ public class PlayerMovement : MonoBehaviour
 
     void OnCollisionEnter(Collision collision)
     {
-        /*
-        Bitwise operation kullanımına bakılmalı, çok daha verimli
-        ve hızlı !!!!!!!!!!!
-        */
         if (collision.gameObject.CompareTag("Ground"))
         {
             if (isSlamming)
@@ -399,7 +369,7 @@ public class PlayerMovement : MonoBehaviour
             }
             if (isWalking) transform.GetChild(0).transform.GetChild(0).gameObject.GetComponent<Animator>().SetBool("isWalking", true);
             isGrounded = true;
-            hasAirDashed = false;  // Yere değdiğinde havada dash durumu sıfırlanır
+            hasAirDashed = false; 
             canDJump = true;
             isJumping = false;
             SfxScript.Instance.playFall();

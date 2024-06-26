f854aaa96044029015bc87d4ec06fc0b6ea5f72a
integerfan66 <kaan27043@gmail.com>
2024-06-26 21:10:26 +0300

    düşmanın bizi duvarın içinden itmesi engellendi

--- Assets/FollowScript.cs
+++ Assets/FollowScript.cs
@@ -2,23 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UIElements;
 
 public class FollowScript : MonoBehaviour
 {
     public bool isFollowing = false;
     public Transform target;
+    bool isTouching = false;
+    NavMeshAgent navMeshAgent;
     // Start is called before the first frame update
     void Start()
     {
-        
+        navMeshAgent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(target != null && isFollowing)
-        this.gameObject.GetComponent<NavMeshAgent>().SetDestination(target.position);
+        if (target != null && isFollowing && !isTouching)
+            navMeshAgent.SetDestination(target.position);
+        else if(isTouching){
+            navMeshAgent.SetDestination(transform.position);
+        }
     }
 
+    void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+            isTouching = true;
 
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+            isTouching = false;
+    }
 }
